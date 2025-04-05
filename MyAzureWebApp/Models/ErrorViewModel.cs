87b28d8c7a7a0ecc78076f0b6402d8d70fe8825e
Devkumar Patel <dpat1072@my.centennialcollege.ca>
2025-04-05 16:29:00 -0400

    Add project files.

---
+++ MyAzureWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAzureWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
